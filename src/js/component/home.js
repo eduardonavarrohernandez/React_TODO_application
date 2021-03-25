9c577c7a70944f42b458d61d4c119a62dddb0b9f
Eduardo G.E <edguidoes@gmail.com>
2021-03-25 23:10:43 +0000

    Added Fetch & Test with Postman

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,23 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 export function Home() {
 	const [tarea, setTarea] = useState("");
 	const [listaTareas, setlistaTareas] = useState([]);
 
+	useEffect(() => {
+		traerTarea();
+	}, []);
+
 	const agregarTarea = () => {
-		if (tarea == "") {
-			alert("Por Favor Agrega una tarea");
-		} else {
-			setlistaTareas([...listaTareas, tarea]);
-			setTarea("");
-		}
+		enviarTarea();
+		setlistaTareas([...listaTareas, { label: tarea, done: false }]);
+		setTarea("");
 	};
+
+	/*----------- */
 	const DeleteItems = indexItem => {
-		setlistaTareas(prevState =>
-			prevState.filter((todo, index) => index !== indexItem)
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		let listaNueva = listaTareas.filter(
+			(todo, index) => index !== indexItem
 		);
+
+		var raw = JSON.stringify(listaNueva);
+
+		var requestOptions = {
+			method: "PUT",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow"
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/edges696",
+			requestOptions
+		)
+			.then(response => response.json())
+			.then(result => traerTarea())
+			.catch(error => console.log("error", error));
 	};
+	/* -------- */
+	function traerTarea() {
+		var requestOptions = {
+			method: "GET",
+			redirect: "follow"
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/edges696",
+			requestOptions
+		)
+			.then(response => response.json())
+			.then(result => setlistaTareas(result))
+			.catch(error => console.log("error", error));
+	}
+
+	/*---enviar tarea */
+	function enviarTarea() {
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify([
+			...listaTareas,
+			{ label: tarea, done: false }
+		]);
+
+		var requestOptions = {
+			method: "PUT",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow"
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/edges696",
+			requestOptions
+		)
+			.then(response => response.json())
+			.then(result => console.log(result))
+			.catch(error => console.log("error", error));
+	}
+
 	return (
 		<div className="col-6 mx-auto text-center bg-white shadow mt-5 rounded pb-5">
 			<div className="row">
@@ -41,7 +106,7 @@ export function Home() {
 						{listaTareas.map((item, index) => {
 							return (
 								<li key={index} className="list-group-item">
-									{item}
+									{item.label}
 									<button
 										className="btn"
 										onClick={() => DeleteItems(index)}>
